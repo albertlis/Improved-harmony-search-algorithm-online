e5ff6fc0d4a51782e5296f7d45c79fec8d69867e
Papa Smerf <albert.lis@interia.pl>
2020-07-06 20:33:55 +0200

    Add plot

--- Vue/hs-frontend/src/views/Result.vue
+++ Vue/hs-frontend/src/views/Result.vue
@@ -2,15 +2,28 @@
     <v-container fill-height>
       <v-row align="center">
         <v-col align="center">
-          
+          <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
         </v-col>
       </v-row>
     </v-container> 
 </template>
 
 <script>
-
+import { Plotly } from 'vue-plotly'
 export default {
   name: 'Result',
+  components: {
+    Plotly
+  },
+  data:() => ({
+    data:[{
+      x: [1,2,3,4],
+      y: [10,15,13,17],
+      type:"scatter"
+    }],
+    layout:{
+      title: "My graph"
+    }
+  })
 }
 </script>
