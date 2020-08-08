ebf8d9114e5d4fca5c8232f3c7401b3547e58be9
PapaSmerf <albert.lis@interia.pl>
2020-08-08 14:21:57 +0200

    Remove Parser.py

--- Vue/hs-frontend/vue.config.js
+++ Vue/hs-frontend/vue.config.js
@@ -1,5 +1,27 @@
+const BrotliGzipPlugin = require('brotli-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = {
   "transpileDependencies": [
     "vuetify"
+  ],
+  configureWebpack: {
+    plugins: [
+      new BrotliGzipPlugin({
+          asset: '[path].br[query]',
+          algorithm: 'br',
+          test: /\.(js|css|html|svg)$/,
+          threshold: 10240,
+          minRatio: 0.8,
+          quality: 11
+      })
+      // new CompressionPlugin({
+      //   asset: '[path].gz[query]',
+      //   test: /\.(js|css)$/,
+      //   algorithm: 'gzip',
+      //   threshold: 10240,
+      //   minRatio: 0.8,
+      //   // deleteOriginalAssets: true
+      // })
   ]
+  }
 }
